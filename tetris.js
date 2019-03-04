27dff67a0cfc88f92f445750b1af6567292698e9
Pavel Beshchuk <PashaBeshchuk1990@gmail.com>
2019-03-04 23:32:49 +0300

    новый алгоритм для I тетрамино

--- tetris.js
+++ tetris.js
@@ -6,58 +6,35 @@ module.exports = {
 	//тетрамино массив из 4 координат [{x: 1, y: 2}, {x: 2, y: 2}, {x: 3, y: 2}, {x: 4, y: 2}]
 	// field это массив двухмерный булин значений, если true в ней есть блок, false блока нет
 	rotateLine: function(field, tetramino){
-		
-		function horizontalPosition_I(tetramino){
-				let arrPosition_Y = [];
+		function rotateLine(tetramino){
+			let arrCoordinates_X  = [];
+			let arrCoordinates_Y  = [];
+			for(let i of tetramino){
+				arrCoordinates_X.push(i.x);
+				arrCoordinates_Y.push(i.y);
+			}
+			if(arrCoordinates_Y[0] == arrCoordinates_Y[1]&& arrCoordinates_Y[2] == arrCoordinates_Y[3]){
+				let a = -1;
 				for(let i of tetramino){
-					arrPosition_Y.push(i.y)
+					i.x = arrCoordinates_X[1];
+					i.y = arrCoordinates_Y[0] + a;
+					a++;
 				}
-				
-				let arrPosition_X = [];
-				for(let i of tetramino){
-					arrPosition_X.push(i.x)
-				}
-				
-				if(arrPosition_Y[0] == arrPosition_Y[1] && arrPosition_Y[2] == arrPosition_Y[3]){
-					for(let i of tetramino){
-						let oldPosition_X = i.x
-						i.x = i.y
-						i.y = oldPosition_X
-					}
-					return tetramino
-				}
-				
-				if(arrPosition_X[0] == arrPosition_X[1] && arrPosition_X[2] == arrPosition_X[3]){
-					for(let i of tetramino){
-						let oldPosition_Y = i.y
-						i.y = i.x
-						i.x = oldPosition_Y
-					}
-					return tetramino
-				}
-				
-
-				
+				return tetramino;
 			}
 			
-		horizontalPosition_I(tetramino)
-		// Тут я определя rotated90Tetramino
-		var rotated90Tetramino = horizontalPosition_I(horizontalPosition_I(tetramino))
-		
-		
-		return horizontalPosition_I(rotated90Tetramino)
-		
-		/* function horizontalPosition_I(tetramino){
+			if(arrCoordinates_X[0] == arrCoordinates_X[1]&& arrCoordinates_X[2] == arrCoordinates_X[3]){
+				let a = -1;
 				for(let i of tetramino){
-					let oldPosition_X = i.x
-					i.x = i.y
-					i.y = oldPosition_X
+					i.y = arrCoordinates_Y[1];
+					i.x = arrCoordinates_X[0] + a;
+					a++;
 				}
-				return tetramino
-			}
-						 
-		return horizontalPosition_I(tetramino) */
-		
+				return tetramino;
+			}		
+		}
+		var tetraminoRotated_90 = rotateLine(rotateLine(tetramino))
+		rotateLine(tetraminoRotated_90)
+		return tetramino
 	}
-	
 }
